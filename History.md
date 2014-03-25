c5f7fef76241a8ea1398242a8e3968d9fe26c0e9
Chilledheart <rwindz0@gmail.com>
2014-03-25 17:23:08 +0800

    ttl and db select support added

---
+++ History.md
@@ -0,0 +1,13 @@
+version 1.1.0
+===
+add select db support and ttl support
+
+version 1.0.1
+===
+
+following koa-session [lastest changes](https://github.com/koajs/session/commit/118a4a8a20c4eed58c61601435e5e73ee774e9cf)
+
+version 1.0
+===
+
+initial commit
--- index.js
+++ index.js
@@ -39,8 +39,8 @@ module.exports = function (opts) {
   debug('redis config port: %s', redisOption.port || (redisOption.port = 6379));
   debug('redis config host: %s', redisOption.host || (redisOption.host = '127.0.0.1'));
   debug('redis config options: %j', redisOption.options || (redisOption.options = {}));
-  debug('redis config db: %s', redisOption.db || (redisOption.db = 0)); //TODO: switch to another db
-  debug('redis config ttl: %s', redisOption.ttl || (redisOption.ttl = null)); //TODO: ttl feature
+  debug('redis config db: %s', redisOption.db || (redisOption.db = 0));
+  debug('redis config ttl: %s', redisOption.ttl);
 
   //redis client for session
   client = redis.createClient(
@@ -48,9 +48,15 @@ module.exports = function (opts) {
     redisOption.host,
     redisOption.options
   );
+
+  client.select(redisOption.db, function () {
+    debug('redis changed to db %i', redisOption.db);
+  });
+
   client.get = thunkify(client.get);
   client.set = thunkify(client.set);
   client.del = thunkify(client.del);
+  client.ttl = redisOption.ttl ? function expire(key) { client.expire(key, redisOption.ttl); }: function () {};
 
   client.on('connect', function () {
     debug('redis is connecting');
@@ -61,6 +67,7 @@ module.exports = function (opts) {
     debug('redis host: %s', client.host);
     debug('redis port: %s', client.port);
     debug('redis parser: %s', client.reply_parser.name);
+    debug('redis server info: %j', client.server_info);
   });
 
   client.on('reconnect', function () {
@@ -68,7 +75,7 @@ module.exports = function (opts) {
   });
 
   client.on('error', function (err) {
-    debug('redis encouters error: %j', err);
+    debug('redis encouters error: %j', err.stack || err);
   });
 
   client.on('end', function () {
@@ -145,6 +152,7 @@ module.exports = function (opts) {
         // save
         json = sess.save();
         yield client.set(sid, json);
+        client.ttl(sid);
       }
     }
   };
